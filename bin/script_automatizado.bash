80fb6d1b6e4a4139c0128f0e21bc6e2ae600e664
Carlos Hernandez <khemtz@gmail.com>
2022-02-25 21:25:45 +0000

    script listo para automatizado

--- bin/script_automatizado.bash
+++ bin/script_automatizado.bash
@@ -20,14 +20,26 @@ ps -ef | egrep 'message|^systemd' >> ${FILEOUTPUT}
 echo "****************************************************"
 
 
-MEM=`free | awk '{print $1";"$2";"$3";"$4";"$5";"$6";"$7}' | tail -2 | head -1`
+MEM=`free -b | awk '{print $1";"$2";"$3";"$4";"$5";"$6";"$7}' | tail -2 | head -1`
 
 TOTAL=`echo ${MEM} | cut -d';' -f2`
 USED=`echo ${MEM} | cut -d';' -f3`
 FREE=`echo ${MEM} | cut -d';' -f4`
 BUFF=`echo ${MEM} | cut -d';' -f6`
 
-FREEPCT=$(((${FREE}/${TOTAL})*100))
+PERC_FREE=`echo "scale=6; 100*(${FREE} / ${TOTAL})" | bc`
 
-echo ${FREEPCT}
-#esc + d + G //borra de donde estoy al final del archivo
+echo "${PERC_FREE}%"
+
+
+if [ ${FREE} -le 500000000 ]
+then
+	echo "ERROR: tes estas quedando sin memoria, ${PERC_FREE}%"	
+else
+	echo "OK"
+fi
+
+
+
+
+
